1e5f93c07df7bc56b0ffe92b88b83a723a6dbc54
jzw13 <jzw13@mails.tsinghua.edu.cn>
2015-07-16 20:20:39 +0800

    Update index.js

--- HW4/index.js
+++ HW4/index.js
@@ -97,7 +97,7 @@ current_longitude = 116;
 //獲取距離
 function getDistance(){
 	debugger;
-	$.getJSON("ext/location.json",function(data)
+	$.getJSON("http://jzw13.github.io/HW4/ext/location.json",function(data)
 		var locations = data.location;)
 	for(i = 0 ; i < info.length ; i++){
 		var StartNS = current_latitude / 180 * Math.PI;
@@ -148,7 +148,7 @@ function getLocation(){
 
 /*function showComment1()
 {
-	$.getJSON("ext/comment_dialog.json",function(data)
+	$.getJSON("http://jzw13.github.io/HW4/ext/comment_dialog.json",function(data)
 		var comments = data.comments;
 		for(i = 0 ; i < comments.length ;i++)
 		{
