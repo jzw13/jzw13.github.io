178d24b9bd244b1456876d494078771c0f175944
jzw13 <jzw13@mails.tsinghua.edu.cn>
2015-07-16 19:02:45 +0800

    = =

--- HW4/index.js
+++ HW4/index.js
@@ -144,7 +144,7 @@ function getLocation(){
 
 function showComment1()
 {
-	$.getJSON(".ext/comment_dialog.json",function(data)
+	$.getJSON("http://jzw13.github.io/HW4/ext/comment_dialog.json",function(data)
 		var comments = data.comments;
 		for(i = 0 ; i < comments.length ;i++)
 		{
