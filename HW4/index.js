6eefa3832aad71b1f7f5f3db6180927898c01557
jzw13 <jzw13@mails.tsinghua.edu.cn>
2015-07-16 22:03:04 +0800

    :)

--- HW4/index.js
+++ HW4/index.js
@@ -125,7 +125,7 @@ function getDistance(){
 function showComment1()
 {	
 	$.getJSON("http://jzw13.github.io/HW4/ext/comment_dialog.json",function(data){
-		var comments = data.comments;
+		var comments = data.review;
 		debugger;
 		for(i = 0 ; i < comments.length ;i++)
 		{
